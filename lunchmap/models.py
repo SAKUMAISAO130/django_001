310d4ef145fff9a413b43b4920f24d9f3c4b80d0
sakuma-tech <colorfullweb@gmail.com>
2020-12-07 13:07:09 +0900

    一旦完成

--- lunchmap/models.py
+++ lunchmap/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -14,17 +15,18 @@ class Category(models.Model):
 
 class Shop(models.Model):
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    address = models.TextField(blank=True)
     author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
     )
     category = models.ForeignKey(
-        Category,
-        on_delete=models.PROTECT,
-    )
+        Category, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('lunchmap:detail', kwargs={'pk': self.pk})
--- lunchmap/views.py
+++ lunchmap/views.py
@@ -4,3 +4,18 @@ from .models import Category, Shop
 
 class IndexView(generic.ListView):
     model = Shop
+
+class DetailView(generic.DetailView):
+    model = Shop
+
+class CreateView(generic.edit.CreateView):
+    model = Shop
+    fields = '__all__'
+
+class UpdateView(generic.edit.UpdateView):
+    model = Shop
+    fields = '__all__'
+
+class DeleteView(generic.edit.DeleteView):
+    model = Shop
+    success_url = reverse_lazy('lunchmap:index')
